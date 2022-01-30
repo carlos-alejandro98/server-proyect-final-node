dd6e211f8a7b78426deb80b1bb379be5feabc238
Carlos Abarca <45491971+carlos-alejandro98@users.noreply.github.com>
2022-01-30 18:02:20 -0300

    Swagger Documentation

--- routes/game.js
+++ routes/game.js
@@ -6,11 +6,24 @@ const { createGame, gamesCount, listAll, softRemoveGame, removeGame, readGame, u
 
 
 // routes-enpoints games
+
 router.post("/game/add", createGame);
 router.get("/games/total", gamesCount);
 router.get("/games/:count", listAll);
 router.patch("/game/softDelete/:slug", softRemoveGame);  // cambio de estado
 router.delete("/game/removeGame/:slug", removeGame); 
+
+/**
+ * @swagger
+ * /game/detailsGame/{game}:
+ *   get:
+ *     tags:
+ *       - name: "Game"
+ *     summary: "Details Game"
+ *     responses:
+ *       200: 
+ *          description: ok   
+ */
 router.get("/game/detailsGame/:slug", readGame);
 router.put("/game/updateGame/:slug", updateGame);
 
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const connectDB = require("./database");
 const morgan = require("morgan");
 const cors = require("cors"); // Protocolo de conexión segura
 const { readdirSync } = require("fs"); // Lectura dinamica de directorios
-//const bodyParser = require("body-parser"); deprecated node >12
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerConfig = require("./documentation/swagger.config.json");
 require("dotenv").config();
 
 // app-server
@@ -18,6 +18,8 @@ connectDB();
 app.use(morgan("dev"));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false }));
+const swaggerDocs = swaggerJsdoc(swaggerConfig);
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs, { explorer: true }));
 app.use(cors());
 
 // routes middlewares-fs option 2 (optime)
